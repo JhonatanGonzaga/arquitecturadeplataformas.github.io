0f8ec7acc4f294ec489b2a35b82ac74a2821c7ff
JhonatanGonzaga <120141875+JhonatanGonzaga@users.noreply.github.com>
2022-12-08 21:00:03 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Arquitectura de Plataformas y Servicios de Tecnologías de Información - Inicio</title>
+<link href="css/estilo.css" rel="stylesheet" type="text/css">
+
+	<!-- Start WOWSlider.com HEAD section --> <!-- add to the <head> of your page -->
+	<link rel="stylesheet" type="text/css" href="engine0/style.css" />
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="engine0/jquery.js"></script>
+	<!-- End WOWSlider.com HEAD section --></head>
+
+<body>
+	<div class="base">
+		<div class="encabezado">
+		<?php include("encabezado.php"); ?>
+		</div>
+		<div class="menu">
+			<?php include("menu.php"); ?>
+		</div>
+		<div class="slider">
+	<!-- Start WOWSlider.com BODY section --> <!-- add to the <body> of your page -->
+	<div id="wowslider-container0">
+	<div class="ws_images"><ul>
+		<li><a href="http://localhost/computacioneinformatica/alumnos.php"><img src="data0/images/sam_0656.jpg" alt=" Semestre II" title=" Semestre II" id="wows0_0"/></a></li>
+		<li><a href="http://localhost/computacioneinformatica/alumnos.php"><img src="data0/images/fb_img_1569183587462.jpg" alt="Semestre IV" title="Semestre IV" id="wows0_1"/></a></li>
+		<li><a href="http://localhost/computacioneinformatica/alumnos.php"><img src="data0/images/img20190704wa0076.jpg" alt="Semestre VI" title="Semestre VI" id="wows0_2"/></a></li>
+		<li><a href="http://localhost/computacioneinformatica/laboratorios.php"><img src="data0/images/sam_0073.jpg" alt="Laboratorio" title="Laboratorio" id="wows0_3"/></a></li>
+		<li><a href="http://localhost/computacioneinformatica/promociones.php"><img src="data0/images/img20190722wa0008.jpg" alt="Promocion 2019" title="Promocion 2019" id="wows0_4"/></a></li>
+		<li><a href="http://localhost/computacioneinformatica/docente.php"><img src="data0/images/img_20190625_101846.jpg" alt="Docente" title="Docente" id="wows0_5"/></a></li>
+		<li><a href="http://localhost/computacioneinformatica/eventos.php"><img src="data0/images/sam_0293.jpg" alt="Eventos" title="Eventos" id="wows0_6"/></a></li>
+	</ul></div>
+	<div class="ws_bullets"><div>
+		<a href="#" title=" Semestre II"><span><img src="data0/tooltips/sam_0656.jpg" alt=" Semestre II"/>1</span></a>
+		<a href="#" title="Semestre IV"><span><img src="data0/tooltips/fb_img_1569183587462.jpg" alt="Semestre IV"/>2</span></a>
+		<a href="#" title="Semestre VI"><span><img src="data0/tooltips/img20190704wa0076.jpg" alt="Semestre VI"/>3</span></a>
+		<a href="#" title="Laboratorio"><span><img src="data0/tooltips/sam_0073.jpg" alt="Laboratorio"/>4</span></a>
+		<a href="#" title="Promocion 2019"><span><img src="data0/tooltips/img20190722wa0008.jpg" alt="Promocion 2019"/>5</span></a>
+		<a href="#" title="Docente"><span><img src="data0/tooltips/img_20190625_101846.jpg" alt="Docente"/>6</span></a>
+		<a href="#" title="Eventos"><span><img src="data0/tooltips/sam_0293.jpg" alt="Eventos"/>7</span></a>
+	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">carousel jquery</a> by WOWSlider.com v8.8</div>
+	<div class="ws_shadow"></div>
+	</div>	
+	<script type="text/javascript" src="engine0/wowslider.js"></script>
+	<script type="text/javascript" src="engine0/script.js"></script>
+	<!-- End WOWSlider.com BODY section -->
+			
+		</div>
+		<div class="cuerpo">
+			<div class="ladoizquierdo">
+		   <div class="docentes">
+			   <div class="imag_docentes">
+			   </div>
+			   <div class="text_docentes">
+				   <a href="docentes.php" target="_blank">DOCENTES>>></a> 
+			   </div>
+		   </div>
+		   <div class="promociones">
+			   <div class="imag_promociones">
+			   </div>
+			   <div class="text_promociones">
+				    <a href="promociones.php" target="_blank">PROMOCIONES>>></a>
+			   </div>
+		   </div>
+		</div>
+		<div class="presentacion">
+		    <div class="titulo_presentacion"></div>
+			<div class="texto_presentacion"></div>		
+		</div>
+		<div class="ladoderecho">
+		  <div class="alumnos">
+			  <div class="imag_alumnos">
+			  </div>
+			  <div class="text_alumnos">
+				   <a href="alumnos.php" target="_blank">ESTUDIANTES>>></a>
+			  
+			  </div>
+		  </div>
+		  <div class="eventos">
+			  <div class="imag_eventos">
+			  </div>
+			  <div class="text_eventos">
+			    <a href="eventos.php" target="_blank"> EVENTOS>>></a>
+			  </div>
+			
+		  </div>
+		</div>
+			
+		</div>
+		<div class="redes">
+			<div class="perfil">
+		<div class="imag_perfil"></div>
+		<div class="text_perfil">
+			<a href="documentos/inicio/Perfil Técnico Profesional.pdf" target="_blank">Perfil Profesional >></a>
+		
+		</div>
+		</div>
+		<div class="facebook">
+		 <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fweb.facebook.com%2FCarrera-Computaci%25C3%25B3n-e-Inform%25C3%25A1tica-IESTP-motupe-113914733340066%2F%3Fmodal%3Dadmin_todo_tour&tabs=timeline&width=650&height=600&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>	
+		</div>
+			<div class="fb-page" data-href="https://web.facebook.com/Carrera-Computaci%C3%B3n-e-Inform%C3%A1tica-IESTP-motupe-113914733340066/?modal=admin_todo_tour" data-tabs="timeline" data-width="500" data-height="600" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://web.facebook.com/Carrera-Computaci%C3%B3n-e-Inform%C3%A1tica-IESTP-motupe-113914733340066/?modal=admin_todo_tour" class="fb-xfbml-parse-ignore"><a href="https://web.facebook.com/Carrera-Computaci%C3%B3n-e-Inform%C3%A1tica-IESTP-motupe-113914733340066/?modal=admin_todo_tour">Carrera Computación e Informática-IESTP &quot;motupe&quot;</a></blockquote></div>			
+	  </div>
+		<div class="pie">
+			<?php include("pie.php"); ?>
+		</div>
+	</div>
+</body>
+</html>
