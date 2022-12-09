a0e9d561ab79a71baf658c883dc5d61ec3c84ada
JhonatanGonzaga <120141875+JhonatanGonzaga@users.noreply.github.com>
2022-12-08 21:02:34 -0500

    Add files via upload

---
+++ acerca.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Arquitectura de Plataformas y Servicios de Tecnologías de Información - Acerca</title>
+<link href="css/acerca.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<div class="base">
+		<div class="encabezado">
+			<?php include("encabezado.php"); ?>
+		
+		</div>
+		<div class="menu">
+			<?php include("menu.php"); ?>
+		</div>
+		
+		<div class="cuerpo">
+		<div class="compu">
+		</div>
+		<div class="historia">
+			<div class="titulohis">
+			</div>
+			<div class="textohis"></div>
+		</div>
+		<div class="jefedearea">
+			<div class="jefe">
+			</div>
+			<div class="oficina"></div>
+		</div>
+		</div>
+		<div class="imagenes">
+		<div class="fachada"></div>
+		<div class="iconos"></div>
+		</div>
+		<div class="pie">
+			<?php include("pie.php"); ?>
+		</div>
+	
+	</div>
+</body>
+</html>
